76b2ea85f9df456fea4e04fab44f3da74fa9ad34
Stewart Davidson <sdavidsonny@outlook.com>
2020-11-20 23:08:36 -0500

    Create Program.cs
    Code Progress for making teams

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        string[] projectMembers;
+        static void Main(string[] args)
+        {
+            string[] projectNames;
+            string[] projectMembers;
+            int codingHours, designHours, 
+                testingHours, projectManagementHours, requirementsAnalysisHours;
+
+            CreateTeamMember();
+          
+        }
+
+        public static void CreateTeamMember()
+        {
+            Console.WriteLine("Enter Number of Team Members in Project");
+            int numberOfMembers = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                Console.WriteLine("Enter Team Member Name");
+                projectMembers [i] = Console.ReadLine();
+            }
+        }
+
+        static void ManageTeamMembers()
+        {
+
+        }
+
+        static void MemberProject()
+        {
+
+        }
+
+        static void HoursExpended()
+        {
+
+        }
+
+    }
+}
