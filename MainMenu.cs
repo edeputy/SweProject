45bac3f4add728ed9c8607103c2ec4b24465cdb5
Beniamin Costea <costea.beny@gmail.com>
2020-11-27 22:24:51 -0500

    Add files via upload
    I emptied Program.cs and moved the main static to MainMenu
    How to operate , in this order so every feature can be tested
    1. Add members ( multiple so you see every feature
    2. Add multiple projects 
    3. List every member
    4. List every project

---
+++ MainMenu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    // This will be the MainMenu where we will be adding new features if necessary
+    public class MainMenu
+    {
+        // Global variables for storing information so that it may be easily accessed 
+        private List<Member> members = new List<Member>();
+        private List<Project> projects = new List<Project>();
+
+        static void Main(string[] args)
+        {
+            // Main Method is called from MainMenu.cs
+            
+            MainMenu menu = new MainMenu();
+
+            // This is the "UI" of our code from Show() method
+            menu.Show();
+
+        }
+
+
+
+        public void Show()
+        {
+            Console.Clear();
+            // IF new feature added, add below code like this
+
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Create Project");
+            Console.WriteLine("2) List Projects (" + this.projects.Count + ")");
+            Console.WriteLine("3) List Members (" +  this.members.Count + ")");
+            Console.WriteLine("4) Add Member");
+            // Console.WriteLine("5) New Feature");
+            Console.Write("\r\nSelect an option: ");
+
+
+            //Switch is the logic behind the menu, this is where you call new features
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Project project = new Project(ref members);
+                    this.projects.Add(project);
+
+                    project.Save();
+
+                    break;
+                case "2":
+                    this.printItems<Project>("Projects", this.projects);
+
+                    break;
+
+                case "3":
+                    this.printItems<Member>("Members", this.members);
+
+                    break;
+
+                case "4":
+                    this.members.Add(new Member(Member.addMemberMenu()));
+
+                    break;
+                
+                default:
+                    break;
+
+            }
+
+            this.Show();
+        }
+        // In case there should be a feature to edit or remove members
+        public void showMemberMenu()
+        {
+
+        }
+
+        // This is a Generic method that prints anything that will be fed to it
+        
+        public void printItems<T>(string heading, List<T> items)
+        {
+            Console.WriteLine("-----");
+            Console.WriteLine(heading +":");
+
+            foreach (T item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("-----");
+            Console.WriteLine("Press enter to return to main menu");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    // This is the logic for "Create Project" in the MainMenu
+    public class Project
+    {
+        //if Risk feature will be added, it should have a property right here
+        public Member owner;
+        public string name;
+        public string description;
+        private List<Member> projectMembers = new List<Member>();
+        private List<Member> globalMembers = new List<Member>();
+        private List<string> requirements = new List<string>();
+
+
+        public Project(ref List<Member> globalMembers)
+        {
+            Console.WriteLine("Owner of the Project");
+            this.owner = new Member(Console.ReadLine());
+
+            Console.WriteLine("Name of the Project");
+            this.name = Console.ReadLine();
+
+            Console.WriteLine("Enter description of the Project");
+            this.description = Console.ReadLine();
+
+            Console.WriteLine("Enter project requirements");
+            this.requirements.AddRange(Console.ReadLine().Split(","));
+
+            // Adding a member to project from the member list that you add from MainMenu
+            Console.WriteLine("Add members to project");
+            int index = 1;
+            foreach (Member item in globalMembers)
+            {
+                Console.WriteLine(index+"."+item.ToString() + "\n");
+                index++;
+               
+            }
+            string selection = Console.ReadLine();
+            Member selectmember = globalMembers[Int32.Parse(selection) - 1];
+            projectMembers.Add(selectmember);
+            // Removing a member from a project should be added below if necessary
+            // should be similiar to adding
+
+
+        }
+        //function for saving information in a file or database
+        public void Save()
+        {
+               
+        }
+        //ading requirement to project functions
+        public void addRequirement(String requirement)
+        {
+            this.requirements.Add(requirement);
+        }
+        
+        public void addMember(Member member)
+        {
+            this.projectMembers.Add(member);
+        }
+
+        public override string ToString()
+        {
+            return (
+                "\t"+this.name + "\n" +
+                "\tOwner: " + owner + "\n" +
+                "\tDescription: " + description + "\n" +
+                "\tMembers (" +projectMembers.Count+"): \n" + GetPrintableList<Member>(projectMembers) + "\n"+
+                "\tRequirements: (" + requirements.Count + "): \n" + GetPrintableList<string>(requirements) + "\n"
+            );
+        }
+        // method for printing generic information
+        public string GetPrintableList<T>(List<T> list)
+        {
+            string res = "";
+
+            foreach(T item in list)
+            {
+                res += "\t\t" + item.ToString() + "\n";
+            }
+
+            return res;
+        }
+    }
+
+
+}
